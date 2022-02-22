b407bfc4feb469ddaffe4af2a7ccd81d7c89af9e
Levi Wilkerson <chelleas123@gmail.com>
2022-02-22 10:02:16 -0500

    style: add mantine providers and init mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,59 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// Init FontAwesome components
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+// Init Mantine Providers for theming and dark/light mode localStorage
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useHotkeys, useLocalStorageValue } from "@mantine/hooks";
+import { NotificationsProvider } from "@mantine/notifications";
+import { AuthProvider } from "../lib/auth/AuthContext";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  // Stores theme data in localStorage
+  const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "dark",
+  });
+
+  // Theme toggler handler
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  // Globally listens for hotkey ctrl+J or cmd+J to toggle theme
+  useHotkeys([
+    [
+      "mod+J",
+      () => {
+        console.log("hotkey pressed");
+        toggleColorScheme();
+      },
+    ],
+  ]);
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        <NotificationsProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
-
-export default MyApp
